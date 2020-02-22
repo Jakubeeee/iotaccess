8ae8c9c95c15d24b3991a54e3b3e04488838fb81
jakubeeee <jakub.bereznicki@gmail.com>
2020-02-22 21:30:45 +0100

    Added module-info.java to plugin-api module

---
+++ plugin-api/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.jakubeeee.masterthesis.pluginapi {
+
+    requires static lombok;
+
+    exports com.jakubeeee.masterthesis.pluginapi;
+    exports com.jakubeeee.masterthesis.pluginapi.config;
+    exports com.jakubeeee.masterthesis.pluginapi.converter;
+    exports com.jakubeeee.masterthesis.pluginapi.meteo;
+    exports com.jakubeeee.masterthesis.pluginapi.property;
+
+}
